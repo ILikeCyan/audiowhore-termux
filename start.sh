d576b221ec989416d7f99cd03d37fa2edb22f967
ILikeCyan <batesjor@deakin.edu.au>
2025-03-31 22:39:29 +1100

    Changes to be committed: deleted:    .config/qobuz-dl/config.ini deleted:    .config/qobuz-dl/qobuz_dl.db modified:   start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 pkg install root-repo git python openssl python-pip zsh && chsh -s zsh
-
 git clone https://github.com/ILikeCyan/audiowhore-termux.git
-
 cp audiowhore-termux/* $HOME/ -r
 cp audiowhore-termux/.* $HOME/ -r
-
 pip install qobuz-dl
+rm -rf .config/qobuz-dl
+echo "Done"
+
+
+
