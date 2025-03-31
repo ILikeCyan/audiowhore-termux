c2d7e7857e1bd199f1cfb68229773f49be9e0455
ILikeCyan <batesjor@deakin.edu.au>
2025-03-31 19:38:37 +1100

    Changes to be committed: 	new file:   start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+pkg install root-repo git python openssl python-pip zsh && chsh -s zsh
+
+git clone https://github.com/ILikeCyan/audiowhore-termux.git
+
+cp audiowhore-termux/* $HOME/ -r
+cp audiowhore-termux/.* $HOME/ -r
+
+pip install qobuz-dl
